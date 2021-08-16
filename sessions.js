89929ce0a24ea5fa324dc542b4241fbd9ac96d80
Alan Martines <alancpmartines@hotmail.com>
2021-08-15 22:37:39 -0400

    Inicial Update

--- sessions.js
+++ sessions.js
@@ -8,7 +8,7 @@ const {
 } = require('p-iteration');
 const axios = require('axios');
 const {
-  WAConnection,
+  clientonnection,
   MessageType,
   Presence,
   MessageOptions,
@@ -17,7 +17,7 @@ const {
   WA_MESSAGE_STUB_TYPES,
   ReconnectMode,
   ProxyAgent,
-  waChatKey,
+  clienthatKey,
 } = require('@adiwajshing/baileys');
 const newinstance = require('./newinstance')
 const mkEvents = require('./events')
@@ -570,7 +570,7 @@ module.exports = class Sessions {
     session.status = "qrRead";
     session.message = 'Sistema iniciando e indisponivel para uso';
     //-------------------------------------------------------------------------------------------------------------------------------------//
-    const client = new WAConnection();
+    const client = new clientonnection();
     client.autoReconnect = true; // auto reconnect on disconnect
     client.logUnhandledMessages = false;
     client.connectOptions = {
@@ -594,7 +594,7 @@ module.exports = class Sessions {
     client.logger.level = 'debug'; // set to 'debug' to see what kind of stuff you can implement
     // attempt to reconnect at most 10 times in a row
     client.connectOptions.maxRetries = 10;
-    client.chatOrderingKey = waChatKey(true); // order chats such that pinned chats are on top
+    client.chatOrderingKey = clienthatKey(true); // order chats such that pinned chats are on top
     //
     let lastqr = null;
     let attempts = 0;
@@ -607,7 +607,7 @@ module.exports = class Sessions {
       console.log('- Número de tentativas de ler o qr-code:', attempts);
       session.attempts = attempts;
       //
-      console.log("- Captura do QR-Code");
+      console.log("- Captura do QR-Code", qr_data);
       //console.log(base64Qrimg);
       session.qrcode = qr_img_buffer;
       //
@@ -685,6 +685,33 @@ module.exports = class Sessions {
     console.log("- Sinstema iniciando");
     var session = Sessions.getSession(SessionName);
     await session.client.then(async (client) => {
+
+      const sharedstate = {}
+      sharedstate.client = client
+
+      const events = mkEvents({
+        SessionName,
+        sharedstate
+      });
+      client.on('blocklist-update', events.blocklistUpdate);
+      client.on('chat-new', events.chatNew);
+      client.on('chats-received', events.chatsReceived);
+      client.on('chat-update', events.chatUpdate);
+      client.on('close', events.close);
+      client.on('connecting', events.connecting);
+      client.on('connection-phone-change', events.connectionPhoneChange);
+      client.on('connection-validated', events.connectionValidated);
+      client.on('contacts-received', events.contactsReceived);
+      client.on('contact-update', events.contactUpdate);
+      client.on('credentials-updated', events.credentialsUpdated);
+      client.on('group-participants-update', events.groupParticipantsUpdate);
+      client.on('group-update', events.groupUpdate);
+      client.on('message-status-update', events.messageStatusUpdate);
+      client.on('open', events.open);
+      client.on('qr', events.qr);
+      //client.on('qr', events.qr_data);
+      client.on('received-pong', events.receivedPong);
+      client.on('ws-close', events.wsClose);
 
       await client.connect().then(() => {
         // credentials are updated on every connect
@@ -1339,7 +1366,7 @@ module.exports = class Sessions {
               "pushname": resultAllContacts.pushname,
               "formattedName": resultAllContacts.formattedName,
               "isMyContact": resultAllContacts.isMyContact,
-              "isWAContact": resultAllContacts.isWAContact,
+              "isclientontact": resultAllContacts.isclientontact,
               "isBusiness": resultAllContacts.isBusiness,
             });
           }
@@ -1390,7 +1417,7 @@ module.exports = class Sessions {
               "name": resultAllContacts.name,
               "formattedName": resultAllContacts.formattedName,
               "isMyContact": resultAllContacts.isMyContact,
-              "isWAContact": resultAllContacts.isWAContact,
+              "isclientontact": resultAllContacts.isclientontact,
               "isBusiness": resultAllContacts.isBusiness,
               "profilePicThumbObj": {
                 "eurl": resultAllContacts.profilePicThumbObj.eurl,
